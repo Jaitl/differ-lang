4e4ded7e12ba772eddcd7ed421d4b7f1cfa95cb9
Jaitl <jaitl@outlook.com>
2016-05-05 22:46:00 +0300

    fix webserver

--- lang-ui/src/main/java/com/jaitlapps/differ/Main.kt
+++ lang-ui/src/main/java/com/jaitlapps/differ/Main.kt
@@ -1,14 +1,15 @@
 package com.jaitlapps.differ
 
+import org.wasabi.app.AppConfiguration
 import org.wasabi.app.AppServer
 import org.wasabi.interceptors.serveStaticFilesFromFolder
 import java.io.File
 
 fun main(args : Array<String>) {
-    var server = AppServer()
+    var server = AppServer(AppConfiguration(port = 8080))
     server.serveStaticFilesFromFolder("WebData${File.separatorChar}public")
 
-    server.get("/", { response.send("Hello World!") })
+    server.get("/", { response.redirect("index.html") })
 
     server.start()
 }
